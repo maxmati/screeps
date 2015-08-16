2c091e0eabcb9adc34d2c77798fc408d01513f9b
Mateusz  "Maxmati" Nowotyński <admin@maxmati.pl>
2015-08-16 13:33:41 +0200

    Clear memory.

--- dist/main.js
+++ dist/main.js
@@ -6,3 +6,9 @@ population();
 if(!Memory.stage)
   Memory.stage = 1;
 // console.log(Game.getUsedCpu());
+
+for(var i in Memory.creeps) {
+    if(!Game.creeps[i]) {
+        delete Memory.creeps[i];
+    }
+}
